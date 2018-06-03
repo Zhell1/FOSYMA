77d04eb3b3de8e106cd15ab271375dfc8ab9cd4e
zhell <thomas.zanivan@gmail.com>
2018-06-04 01:17:39 +0200

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "pret pour retirer silonode avant dijkstra""""""""""""
    This reverts commit 2db1eb47f827d987764b1be2dc4fd47d8e4d60ec.

--- src/princ/Principal.java
+++ src/princ/Principal.java
@@ -40,13 +40,13 @@ public class Principal {
 
 		System.out.println("Hello !");
 		//0) Create the real environment and the observed one
-		//env= new Environment(ENVtype.GRID_T,5,null);
+		env= new Environment(ENVtype.GRID_T,5,null);
 		//env= new Environment(ENVtype.DOROGOVTSEV_T,50,null);
 		//env= new Environment(ENVtype.DOROGOVTSEV_T,8,null);
 		//env=new Environment("ressources/map2017-2","ressources/map2017-config");
 		//env=new Environment("ressources/mapzhell","ressources/mapzhell.config");
 		
-		env=new Environment("ressources/map2018","ressources/map2018-multiType-exam");
+		//env=new Environment("ressources/map2018","ressources/map2018-multiType-exam");
 
 		//1), create the platform (Main container (DF+AMS) + containers + monitoring agents : RMA and SNIFFER)
 		rt=emptyPlatform(containerList);
